197c560a8503d10f9fb7b32ccf0924396496004e
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-05 15:17:27 -0300

    Adicionar arquivos de projeto.

---
+++ Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*01. Escreva um programa que leia uma string e um caractere, e retorne o número de vezes que esse caractere aparece na string.*/
+        }
+    }
+}
