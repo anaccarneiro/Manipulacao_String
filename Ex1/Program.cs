526bceaea52b6293b0bc10d0863654112c8229f0
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-05 15:49:32 -0300

    Ex1 Finalizado

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.Design.Serialization;
 
 namespace Ex1
 {
@@ -7,6 +8,22 @@ namespace Ex1
         static void Main(string[] args)
         {
             /*01. Escreva um programa que leia uma string e um caractere, e retorne o número de vezes que esse caractere aparece na string.*/
+            string palavra;
+            char letra;
+            int cont = 0;
+
+            Console.WriteLine("Digite uma palavra:");
+            palavra = Console.ReadLine();
+            Console.WriteLine("Informe uma letra presente na palavra:");
+            letra = char.Parse(Console.ReadLine());
+
+            foreach (char caracter in palavra)
+            {
+                if (letra == caracter)
+                    cont++;
+            }
+
+            Console.WriteLine($"A letra '{letra}' aparece {cont} vez(es) em '{palavra}'.");
         }
     }
 }
