9d247f3626100fe73e703de1e9acbc86ace850b9
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-06 16:35:44 -0300

    Ex4 Finalizado

---
+++ Ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*04. Escreva um programa que leia uma palavra e a imprima de trás-para-frente.*/
+
+            string word, invertida = "a";
+            Console.WriteLine("Digite uma palavra");
+            word = Console.ReadLine();
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                Console.Write(word[i]);
+            }
+        }
+    }
+}
