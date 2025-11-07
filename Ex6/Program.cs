d44115d42ae8ff913f5a8d630e71d2a0ab841149
Ana Clara <acccarneiro@sga.pucminas.br>
2025-11-07 14:37:06 -0300

    Ex6 Finalizado

---
+++ Ex6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Principal;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*06. Faça um programa para criptografar uma frase informada pelo usuário. A criptografia deverá trocar as vogais da
+            frase por *. Exemplo:
+            Frase: EU ESTOU NA ESCOLA
+            Saída: ** *ST** N* *SC*L**/
+
+            string frase = "", criptografar = "";
+            char [] vogais = {'a','e','i','o','u'};
+            Console.WriteLine("Escreva a frase desejada:");
+            frase = Console.ReadLine().ToLower();
+
+            criptografar = frase;
+
+            foreach (char letra in frase)
+            {
+                bool igualdade = (vogais.Contains(letra));
+                if (igualdade)
+                {
+                    criptografar = criptografar.Replace(letra, '*');
+                }
+            }
+
+            Console.Write(criptografar);
+        }
+    }
+}
